d4726ec40698765b3a4cc1fef0eaa497213cf017
FanggeXiong <fanggx@uw.edu>
2019-05-14 16:20:55 -0700

    exercise

--- API_lab.R
+++ API_lab.R
@@ -1,22 +1,28 @@
 ## What libraries do we need to load? 
+library("httr")
+library("jsonlite")
 
 ## Source in your hidden api-keys.R file
-
+source("key.R")
 
 ##  Let's construct the API call.
 # base_uri <-
+base_url <- AIzaSyDqbQXLIixojq2yFjvWWJ1yIj83IPxtcjY
 
 # resource <-
+resource <- "/representatives"
 
 # query_params <- list(
   # what (optional) parameters do we need?
 # )
 
-# response <-
+query_params <- list( key = api_key, address = "1315 NE Campus")
 
-# response_content <-
+response <- GET(paste(base_url,resource), query = query_params)
 
-# parsed_data <-
+response_content <- content(response, "text")
+
+parsed_data <- fromJSON(response_content)
 
 
 ## Yay! Now we should have our data. Let's View(parsed_data) to see what we have to work with.
